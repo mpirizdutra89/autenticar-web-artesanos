a483b579f1cbc5d193c5560dcfd83beebdcfc9da
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2025-06-18 02:13:47 -0300

    Update db.js

--- db.js
+++ db.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 // Crea un pool de conexiones a la base de datos
 // Un pool es más eficiente que abrir y cerrar una conexión para cada consulta.
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',     // Host de la base de datos (ej. 'localhost')
-    user: process.env.DB_USER || 'root',     // Usuario de la base de datos
-    password: process.env.DB_PASSWORD || 'nicolas89', // Contraseña del usuario de la base de datos
-    database: process.env.DB_NAME || 'mpd_artesanos',   // Nombre de la base de datos
+    host: process.env.DB_HOST ? process.env.DB_HOST : 'localhost',     // Host de la base de datos (ej. 'localhost')
+    user: process.env.DB_USER ? process.env.DB_USER : 'root',     // Usuario de la base de datos
+    password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : 'reSUlu43ra', // Contraseña del usuario de la base de datos
+    database: process.env.DB_NAME ? process.env.DB_NAME : 'mpd_artesanos',   // Nombre de la base de datos
     waitForConnections: true,      // Si no hay conexiones disponibles, espera
     connectionLimit: 10,           // Número máximo de conexiones en el pool
     queueLimit: 0                  // Límite de solicitudes en cola (0 = sin límite)
